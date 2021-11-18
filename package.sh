c78bd414c2160409db3207a56186e57b4f293a1c
Christoph Vögele <christoph.voegele@fhnw.ch>
2021-11-18 14:40:51 +0100

    update

--- package.sh
+++ package.sh
@@ -6,7 +6,7 @@ mkdir package-$RUNNER_OS/oskar
 cd workspace
 git clone https://github.com/OxfordSKA/OSKAR.git .
 cd package-$RUNNER_OS/oskar
-cmake ~/workspace/. -DFIND_CUDA=OFF #-DCMAKE_INSTALL_PREFIX=../../package/oskar
+cmake ../../workspace/. -DFIND_CUDA=OFF #-DCMAKE_INSTALL_PREFIX=../../package/oskar
 make -j4
 cd ../..
 tar -zcf oskar-binaries-$RUNNER_OS-x86_64.tar.gz package-$RUNNER_OS
