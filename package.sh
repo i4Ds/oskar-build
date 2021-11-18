786ef5d8181b6ac90f7d3cfdae4e3f7a98f88a0c
Christoph Vögele <christoph.voegele@fhnw.ch>
2021-11-18 13:53:24 +0100

    add install call

--- package.sh
+++ package.sh
@@ -2,11 +2,13 @@
 mkdir binaries
 mkdir workspace
 cd workspace
+mkdir install_dir
 git clone https://github.com/OxfordSKA/OSKAR.git .
 mkdir build
 cd build
-cmake .. -DFIND_CUDA=OFF
+cmake .. -DFIND_CUDA=OFF -DCMAKE_INSTALL_PREFIX=../install_dir
 make -j4
+make install
 cd ..
-tar -zcf ../oskar-binaries-$RUNNER_OS-x86_64.tar.gz ./build
+tar -zcf ../oskar-binaries-$RUNNER_OS-x86_64.tar.gz ./install_dir
 ls ../
