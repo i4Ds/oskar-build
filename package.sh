e81c5c51f71242f4a3ead592ff51ccd84201d392
Christoph Vögele <christoph.voegele@fhnw.ch>
2021-11-18 14:26:28 +0100

    add python wheel build

--- package.sh
+++ package.sh
@@ -1,14 +1,20 @@
 
 mkdir binaries
 mkdir workspace
-mkdir install_dir
+mkdir package
+mkdir package/oskar
 cd workspace
 git clone https://github.com/OxfordSKA/OSKAR.git .
 mkdir build
 cd build
-cmake .. -DFIND_CUDA=OFF -DCMAKE_INSTALL_PREFIX=../../install_dir
+cmake .. -DFIND_CUDA=OFF -DCMAKE_INSTALL_PREFIX=../../package/oskar
 make -j4
 make install
+cd ..
+cd python
+export OSKAR_INC_DIR=../../package/oskar
+export OSKAR_LIB_DIR=../../package/oskar
+python3 setup.py bdist_wheel -w ../../package
 cd ../..
 tar -zcf oskar-binaries-$RUNNER_OS-x86_64.tar.gz install_dir
 ls ../
