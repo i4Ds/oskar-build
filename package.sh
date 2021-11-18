71966eb1f29f7a12cb3ab059f37c786c9efabce7
Christoph Vögele <christoph.voegele@fhnw.ch>
2021-11-18 11:19:24 +0100

    add packaing script

---
+++ package.sh
@@ -0,0 +1,12 @@
+
+mkdir binaries
+mkdir workspace
+cd workspace
+git clone https://github.com/OxfordSKA/OSKAR.git .
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX="../binaries" -DFIND_CUDA=OFF
+make -j4
+cd ..
+
+tar -zcvf oskar-binaries-linux-x86_64.tar.gz /binaries
