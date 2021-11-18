d8caca23b1126e9cb0c9c883405fc4cda876c74f
Christoph Vögele <christoph.voegele@fhnw.ch>
2021-11-18 11:41:43 +0100

    Update package.sh

--- package.sh
+++ package.sh
@@ -5,7 +5,9 @@ cd workspace
 git clone https://github.com/OxfordSKA/OSKAR.git .
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX="../binaries" -DFIND_CUDA=OFF
+cmake .. -DFIND_CUDA=OFF
 make -j4
+cd ..
+ls
 tar -zcvf oskar-binaries-linux-x86_64.tar.gz ./build
 
