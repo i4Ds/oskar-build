7010705254d9b2fd43432347de279a3e396ddf99
Christoph Vögele <christoph.voegele@fhnw.ch>
2021-11-18 14:38:27 +0100

    remove python wheel build

--- package.sh
+++ package.sh
@@ -1,20 +1,13 @@
 
 mkdir binaries
 mkdir workspace
-mkdir package
-mkdir package/oskar
+mkdir package-$RUNNER_OS
+mkdir package-$RUNNER_OS/oskar
 cd workspace
 git clone https://github.com/OxfordSKA/OSKAR.git .
-mkdir build
-cd build
-cmake .. -DFIND_CUDA=OFF -DCMAKE_INSTALL_PREFIX=../../package/oskar
+cd package/oskar
+cmake ~/workspace/. -DFIND_CUDA=OFF #-DCMAKE_INSTALL_PREFIX=../../package/oskar
 make -j4
-make install
-cd ..
-cd python
-export OSKAR_INC_DIR=../../package/oskar
-export OSKAR_LIB_DIR=../../package/oskar
-python3 setup.py bdist_wheel -w ../../package
 cd ../..
-tar -zcf oskar-binaries-$RUNNER_OS-x86_64.tar.gz install_dir
+tar -zcf oskar-binaries-$RUNNER_OS-x86_64.tar.gz package-$RUNNER_OS
 ls ../
