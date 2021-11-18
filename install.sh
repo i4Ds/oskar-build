91cab8a94d1ca733334d11d3952ea68731fd4e54
Christoph Vögele <christoph.voegele@fhnw.ch>
2021-11-18 17:01:59 +0100

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 mkdir ~/oskar
 cp -r oskar/ ~/oskar
-export OSKAR_LIB_DIR="~/oskar/lib"
+#export OSKAR_LIB_DIR="~/oskar/lib"
 export OSKAR_INC_DIR="~/oskar/include"
-pip3 install --user 'git+https://github.com/OxfordSKA/OSKAR.git@master#egg=oskarpy&subdirectory=python'
+pip3 install --user --global-option=build_ext --global-option="-L~/oskar/lib" 'git+https://github.com/OxfordSKA/OSKAR.git@master#egg=oskarpy&subdirectory=python' --library-dirs
