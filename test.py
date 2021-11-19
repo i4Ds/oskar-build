f9e850106dec122273a53845b271389e10bee2c4
Christoph Vögele <christoph.voegele@fhnw.ch>
2021-11-19 11:46:40 +0100

    update test case with ms file

--- test.py
+++ test.py
@@ -1,9 +1,45 @@
 import oskar
 import numpy as np
 
-sky_data = np.array([
+
+def testSimulation:
+    sky_data = np.array([
         [20.0, -30.0, 1, 0, 0, 0, 100.0e6, -0.7, 0.0, 0,   0,   0],
         [20.0, -30.5, 3, 2, 2, 0, 100.0e6, -0.7, 0.0, 600, 50,  45],
         [20.5, -30.5, 3, 0, 0, 2, 100.0e6, -0.7, 0.0, 700, 10, -10]])
 
-sky = oskar.Sky.from_array(sky_data, "single")
+    sky = oskar.Sky.from_array(sky_data, "single")
+
+    params = {
+        "simulator": {
+            "use_gpus": False
+            "double_precision" = False
+        },
+        "observation" : {
+            "num_channels": 64,
+            "start_frequency_hz": 100e6,
+            "frequency_inc_hz": 20e6,
+            "phase_centre_ra_deg": 20,
+            "phase_centre_dec_deg": -30,
+            "num_time_steps": 24,
+            "start_time_utc": "01-01-2000 12:00:00.000",
+            "length": "12:00:00.000"
+        },
+        "telescope": {
+            "input_directory": "../data/telescope.tm"
+        },
+        "interferometer": {
+            "ms_filename": "visibilities.ms",
+            "channel_bandwidth_hz": 1e6,
+            "time_average_sec": 10
+        }
+    }
+
+    settings = oskar.SettingsTree("oskar_sim_interferometer")
+    settings.from_dict(params)
+
+    # Set the sky model and run the simulation.
+    sim = oskar.Interferometer(settings=settings)
+
+
+testSimulation()
