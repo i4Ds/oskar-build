029b4bfc3215c694b0c90d6aeddc9b5bc8df694f
Christoph Vögele <christoph.voegele@fhnw.ch>
2021-11-19 11:27:21 +0100

    update precision in test

--- test.py
+++ test.py
@@ -6,4 +6,4 @@ sky_data = np.array([
         [20.0, -30.5, 3, 2, 2, 0, 100.0e6, -0.7, 0.0, 600, 50,  45],
         [20.5, -30.5, 3, 0, 0, 2, 100.0e6, -0.7, 0.0, 700, 10, -10]])
 
-sky = oskar.Sky.from_array(sky_data, precision)
+sky = oskar.Sky.from_array(sky_data, "single")
