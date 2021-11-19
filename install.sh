e78ffb25864cf1eb27e90fafb95801ec5a5031b1
Christoph Vögele <christoph.voegele@fhnw.ch>
2021-11-19 11:33:32 +0100

    update

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ echo "ls"
 ls $HOME/oskar
 export OSKAR_LIB_DIR="$HOME/oskar/lib"
 export OSKAR_INC_DIR="$HOME/oskar/include"
-git clone https://github.com/i4Ds/OSKAR.git
-cd OSKAR/python
-python3 setup.py install --user --prefix=
-#pip3 install 'git+https://github.com/i4Ds/OSKAR.git@master#egg=oskarpy&subdirectory=python'
+#git clone https://github.com/i4Ds/OSKAR.git
+#cd OSKAR/python
+#python3 setup.py install --user --prefix=
+pip3 install --user 'git+https://github.com/i4Ds/OSKAR.git@master#egg=oskarpy&subdirectory=python'
 #pip3 install --user --global-option=build_ext --global-option="-L~/oskar/lib" 'git+https://github.com/i4Ds/OSKAR.git@master#egg=oskarpy&subdirectory=python'
