cdc5a9de96722aeaac0a52352f8df7b6184f71cc
Christoph Vögele <christoph.voegele@fhnw.ch>
2021-11-19 16:30:31 +0100

    update tests

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 wget -O data.zip https://github.com/OxfordSKA/OSKAR/files/1984210/OSKAR-2.7-Example-Data.zip
 ls
 unzip data.zip
-cp -r data/telescope.tm telescope.tm
+cp -r OSKAR-2.7-Example-Data/telescope.tm .
 
 python3 test.py
