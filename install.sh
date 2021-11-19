90288723d9777ff9b62d00b6beb999d26cb529dd
Christoph Vögele <christoph.voegele@fhnw.ch>
2021-11-19 10:52:49 +0100

    add setup.py

--- install.sh
+++ install.sh
@@ -2,5 +2,8 @@ mkdir ~/oskar
 cp -r oskar/ $HOME/oskar
 export OSKAR_LIB_DIR="$HOME/oskar/lib"
 export OSKAR_INC_DIR="$HOME/oskar/include"
-pip3 install 'git+https://github.com/i4Ds/OSKAR.git@master#egg=oskarpy&subdirectory=python'
+git clone https://github.com/i4Ds/OSKAR.git
+cd OSKAR/python
+python3 setup.py install
+#pip3 install 'git+https://github.com/i4Ds/OSKAR.git@master#egg=oskarpy&subdirectory=python'
 #pip3 install --user --global-option=build_ext --global-option="-L~/oskar/lib" 'git+https://github.com/i4Ds/OSKAR.git@master#egg=oskarpy&subdirectory=python'
