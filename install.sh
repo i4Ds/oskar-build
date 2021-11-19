66c6e181525f644903dfdfef702ed838528f610e
Christoph Vögele <christoph.voegele@fhnw.ch>
2021-11-19 10:57:12 +0100

    add user flag

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ export OSKAR_LIB_DIR="$HOME/oskar/lib"
 export OSKAR_INC_DIR="$HOME/oskar/include"
 git clone https://github.com/i4Ds/OSKAR.git
 cd OSKAR/python
-python3 setup.py install
+python3 setup.py install --user --prefix=
 #pip3 install 'git+https://github.com/i4Ds/OSKAR.git@master#egg=oskarpy&subdirectory=python'
 #pip3 install --user --global-option=build_ext --global-option="-L~/oskar/lib" 'git+https://github.com/i4Ds/OSKAR.git@master#egg=oskarpy&subdirectory=python'
