777d19453a6e864976840defaa71788a9cf52f1b
Christoph Vögele <christoph.voegele@fhnw.ch>
2021-11-22 07:56:58 +0100

    update

--- package.sh
+++ package.sh
@@ -1,28 +1,33 @@
-mkdir binaries
-mkdir workspace
-mkdir package-$RUNNER_OS
-mkdir package-$RUNNER_OS/oskar
+#!/bin/sh
+
+#Set directories
+OSKAR_SRC_DIR=$HOME/workspace/oskar
+OSKAR_BUILD_DIR=$HOME/workspace/oskar/build
+OSKAR_INSTALL_DIR=$HOME/package-$RUNNER_OS/oskar
+CASACORE_INSTALL_DIR=$HOME/workspace/casacore
+
+mkdir -p $OSKAR_SRC_DIR
+mkdir -p $OSKAR_BUILD_DIR
+mkdir -p $OSKAR_INSTALL_DIR
+mkdir -p $CASACORE_INSTALL_DIR
 
 #install casacore
-cd workspace
-mkdir casacore
-cd casacore
+cd $CASACORE_INSTALL_DIR
 apt-get download casacore-dev
-cd ..
+ls
 
 #install oksar
-mkdir oskar
-git clone https://github.com/OxfordSKA/OSKAR.git oskar/.
-cd oskar
-mkdir build
-cd build
-cmake ../../workspace/. -DFIND_CUDA=OFF -DCMAKE_INSTALL_PREFIX=../../package-$RUNNER_OS/oskar
+cd $OSKAR_SRC_DIR
+git clone https://github.com/OxfordSKA/OSKAR.git .
+cd $OSKAR_BUILD_DIR
+cmake $OSKAR_SRC_DIR -DFIND_CUDA=OFF -DCMAKE_INSTALL_PREFIX=$OSKAR_INSTALL_DIR
 make -j4
 make install
 ctest
-cd ../../..
+
+cd $HOME
 cp install.sh package-$RUNNER_OS/
 cp test.sh package-$RUNNER_OS/
 cp test.py package-$RUNNER_OS/
 tar -zcf oskar-binaries-$RUNNER_OS-x86_64.tar.gz package-$RUNNER_OS
-ls ../
+
