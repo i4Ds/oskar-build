58cfb5b41839ea5217374bdf7d5521ab10098eda
Christoph Vögele <christoph.voegele@fhnw.ch>
2021-11-22 10:11:27 +0100

    update

--- package.sh
+++ package.sh
@@ -17,7 +17,7 @@ dpkg-deb -R casacore-dev*.deb $CASACORE_PACKAGE_DIR
 CASACORE_INC_DIR=$CASACORE_PACKAGE_DIR/usr/include/casacore
 CASACORE_LIB_DIR=$CASACORE_PACKAGE_DIR/usr/lib/x86_64-linux-gnu
 #CASACORE_LIBRARIES=$CASACORE_LIB_DIR
-echo $CASACORE_LIB_DIR
+ls $CASACORE_LIB_DIR
 echo $CASACORE_INC_DIR
 
 echo "*- download oskar -------------------------------------------------------------------------- *"
