45f16c0f740fb36109c7844c77a05ce78430da69
Christoph Vögele <christoph.voegele@fhnw.ch>
2021-11-22 10:51:12 +0100

    update

--- package.sh
+++ package.sh
@@ -13,7 +13,7 @@ mkdir -p $CASACORE_DOWNLOAD_DIR
 mkdir -p $CASACORE_PACKAGE_DIR
 cd $CASACORE_DOWNLOAD_DIR
 apt-get update
-apt-get install casacore-dev
+apt-get install -y casacore-dev
 #apt-get download casacore-dev
 #dpkg-deb -R casacore-dev*.deb $CASACORE_PACKAGE_DIR
 #CASACORE_INC_DIR=$CASACORE_PACKAGE_DIR/usr/include/casacore
