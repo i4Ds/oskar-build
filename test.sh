1d1f8f2da5ff528a4392d429d1eb1c5aa3a5eaa0
Christoph Vögele <christoph.voegele@fhnw.ch>
2021-11-22 13:24:24 +0100

    🔨

--- test.sh
+++ test.sh
@@ -4,4 +4,7 @@ wget -O data.zip https://github.com/OxfordSKA/OSKAR/files/1984210/OSKAR-2.7-Exam
 unzip data.zip
 cp -r OSKAR-2.7-Example-Data/telescope.tm .
 
+dir=$(python3 -m site --user-site)
+ls $dir
+
 python3 test.py
