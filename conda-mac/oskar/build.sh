e7981d9277baf9e73cfbfee9c64f6f718e77e67f
cvoegele <christoph228@protonmail.ch>
2022-02-08 06:38:25 +0000

    update

---
+++ conda-mac/oskar/build.sh
@@ -0,0 +1,8 @@
+mkdir -p $PREFIX
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX
+make -j install
+
+export OSKAR_INC_DIR=$PREFIX/include
+export OSKAR_LIB_DIR=$PREFIX/lib
+
+cd $SRC_DIR/python && $PYTHON -m pip install .  -vv
