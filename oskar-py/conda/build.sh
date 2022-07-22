a86aaa6fb5d9ffba3db434cf902ee867b16ebb5c
Christoph Vögele <christoph.voegele@fhnw.ch>
2022-07-22 08:55:28 +0200

    update oskar clone path

--- oskar-py/conda/build.sh
+++ oskar-py/conda/build.sh
@@ -1,4 +1,4 @@
-git clone -b $PKG_VERSION https://github.com/OxfordSKA/OSKAR.git .
+git clone -b $PKG_VERSION https://github.com/OxfordSKA/OSKAR.git
 
-cd python
+cd oskar/python
 $PYTHON -m pip install --no-deps .
