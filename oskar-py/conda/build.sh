96d239577a3ece798829ebcbea727fcda246c0dd
Christoph Vögele <christoph.voegele@fhnw.ch>
2022-07-22 12:00:42 +0200

    add prefix

--- oskar-py/conda/build.sh
+++ oskar-py/conda/build.sh
@@ -1,4 +1,8 @@
 git clone -b $PKG_VERSION https://github.com/OxfordSKA/OSKAR.git
 
 cd OSKAR/python
+
+export OSKAR_INC_DIR=$PREFIX/include
+export OSKAR_LIB_DIR=$PREFIX/lib
+
 $PYTHON -m pip install --no-deps .
