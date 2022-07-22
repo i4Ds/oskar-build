1b14ed629018a285de68dfd0df222d6b08cec36c
cvoegele <christoph228@protonmail.ch>
2022-07-22 13:37:30 +0000

    update

--- oskar-py/conda/build.sh
+++ oskar-py/conda/build.sh
@@ -1,8 +1,4 @@
-git clone -b $PKG_VERSION https://github.com/OxfordSKA/OSKAR.git
+git clone https://github.com/i4Ds/OSKAR.git
 
 cd OSKAR/python
-
-export OSKAR_INC_DIR=$PREFIX/include
-export OSKAR_LIB_DIR=$PREFIX/lib
-
 $PYTHON -m pip install --no-deps .
