5604698fd0b30bb6c4490bfc573bcea09a9f2d0d
lukas.gehrig <lukas.gehrig@students.fhnw.ch>
2022-10-14 11:41:12 +0200

    changed oskar-py repo cloning to oskar native and set env vars instead

--- oskar-py/conda/build.sh
+++ oskar-py/conda/build.sh
@@ -3,7 +3,10 @@ wget https://github.com/i4Ds/OSKAR/blob/master/LICENSE
 
 pkgv=$(sed 's/dev//g' <<< "$PKG_VERSION")
 
-git clone --branch "$pkgv" https://github.com/i4Ds/OSKAR.git
+git clone --branch "$pkgv" https://github.com/OxfordSKA/OSKAR.git
+
+export OSKAR_INC_DIR="$PREFIX"/include
+export OSKAR_LIB_DIR="$PREFIX"/lib
 
 cd OSKAR/python
 $PYTHON -m pip install --no-deps .
