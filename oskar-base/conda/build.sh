265c7befad38d444d5d5b24709f0a2585871aec2
vincenzo.timmel <vincenzo.timmel@gmail.com>
2022-12-06 19:16:42 +0100

    Updated to add GPU Support.

--- oskar-base/conda/build.sh
+++ oskar-base/conda/build.sh
@@ -8,14 +8,7 @@ echo $CUDA_PATH
 mkdir tmp
 cd tmp
 git clone --branch "$pkgv" https://github.com/OxfordSKA/OSKAR.git .
-cmake -DCFIND_CUDA=ON -DCMAKE_INSTALL_PREFIX=$PREFIX -DCUDA_ARCH="ALL"
-make -j install
+cmake -DCFIND_CUDA=ON -DCMAKE_INSTALL_PREFIX=$PREFIX -DCUDA_ARCH="ALL;8.0:8.6;8.7"
+make -j2 install
 cd ..
 rm -rf tmp
-#
-#export OSKAR_INC_DIR=$CONDA_PREFIX/include
-#export OSKAR_LIB_DIR=$CONDA_PREFIX/lib
-#
-#mkdir -p $PREFIX
-#cmake -DCFIND_CUDA=OFF -DCUDA_ARCH="3.5;3.7;5.0;5.2;6.0;6.1;6.2;7.0;7.5" -DNVCC_COMPILER_BINDIR=$CXX -DCMAKE_INSTALL_PREFIX=$PREFIX
-#cd $SRC_DIR/python && $PYTHON -m pip install .  -vv
