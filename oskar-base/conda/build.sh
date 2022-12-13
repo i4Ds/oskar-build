026472e73e210ce2b8a910373266ee157fae8d61
Vincenzo Timmel <vincenzo.timmel@gmail.com>
2022-12-13 16:06:52 +0100

    Replaced 'all' key.

--- oskar-base/conda/build.sh
+++ oskar-base/conda/build.sh
@@ -3,12 +3,10 @@
 wget https://github.com/OxfordSKA/OSKAR/blob/master/LICENSE
 
 pkgv=$(sed 's/dev//g' <<< "$PKG_VERSION")
-echo $pkgv
-echo $CUDA_PATH
 mkdir tmp
 cd tmp
 git clone --branch "$pkgv" https://github.com/OxfordSKA/OSKAR.git .
-cmake -DCFIND_CUDA=ON -DCMAKE_INSTALL_PREFIX=$PREFIX -DCUDA_ARCH="ALL;8.0;8.6;8.7"
+cmake -DCFIND_CUDA=ON -DCMAKE_INSTALL_PREFIX=$PREFIX -DCUDA_ARCH="6.0;6.1;6.2;7.0;7.5;8.0;8.6;8.7"
 make -j2 install
 cd ..
 rm -rf tmp
